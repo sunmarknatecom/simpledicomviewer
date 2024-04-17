90dfc2a2006d875e34a348dcc63667d3fab1f164
Mark S. Hong <33437440+sunmarknatecom@users.noreply.github.com>
2024-04-17 14:11:43 +0900

    function to menubar, create scrollbar for size fixation between frame and canvas

--- sdv005.py
+++ sdv005.py
@@ -1,68 +1,105 @@
 import tkinter as tk
-from PIL import Image, ImageTk
 import pydicom, os
 import numpy as np
-from tkinter import filedialog
+from PIL import Image, ImageTk
+from tkinter import filedialog, ttk
 from glob import glob
 
 class SimpleDicomViewer:
     def __init__(self, root):
+        '''
+        Title: SimpleDicomViewer
+        init function-
+        root
+        L___menubar                             |
+        L___frame(grid) canvas in frame         |
+            L____pre_btn --- slider --- nxt_btn |
+        '''
         self.root = root
         self.root.title("Simple DICOM Viewer")
-        self.menubar = tk.Menu(root)
-        #---menu1---#
-        self.menu1 = tk.Menu(self.menubar, tearoff=0)
-        self.menu1.add_command(label="Open", command=self.open_dicom)
-        self.menu1.add_command(label="Open_dir", command=self.open_folder)
-        self.menu1.add_command(label="Close", command=self.close)
-        #---menu2---#
-        self.menu2 = tk.Menu(self.menubar, tearoff=0)
-        self.menu2.add_command(label="Invert", command=self.invert)
-        #---menu3---#
-        self.menu3 = tk.Menu(self.menubar, tearoff=0)
-        self.menu3.add_command(label="Info", command=self.show_info_window)
-        #---menu4---#
-        self.menu4 = tk.Menu(self.menubar, tearoff=0)
-        self.menu4.add_command(label="Cont HU", command=self.show_control_HU_window)
-        #---menubar---#
-        self.menubar.add_cascade(label="File",menu=self.menu1)
-        self.menubar.add_cascade(label="Color", menu=self.menu2)
-        self.menubar.add_cascade(label="ContHU", menu=self.menu4)
-        self.menubar.add_cascade(label="Help", menu=self.menu3)
-        self.root.config(menu=self.menubar)
+        self.create_menu()
+        #----frame----#
         self.frame = tk.Frame(self.root)
         self.frame.grid(row=0, column=0, columnspan=3)
-        self.canvas = tk.Canvas(self.frame, bg="white", width=1024, height=1024)
+        #----canvas----#
+        self.canvas = tk.Canvas(self.frame, bg="white", width=512, height=768)
         self.canvas.grid(row=0, column=0, columnspan=3)
+        #----scrollbar_vertical---#
+        self.scrollbar_vertical = ttk.Scrollbar(self.frame, orient="vertical", command=self.canvas.yview)
+        self.scrollbar_vertical.grid(row=0,column=3, sticky="sn")
+        self.canvas.configure(yscrollcommand=self.scrollbar_vertical.set)
+        #----scrollbar_horizontal----#
+        self.scrollbar_horizontal = ttk.Scrollbar(self.frame, orient="horizontal", command=self.canvas.xview)
+        self.scrollbar_horizontal.grid(row=1,column=0, columnspan=3, sticky="ew")
+        self.canvas.configure(xscrollcommand=self.scrollbar_horizontal.set)
         #----button---#
         self.prev_button = tk.Button(self.frame, text="PREV", width=7, command=self.show_previous_image)
-        self.prev_button.grid(row=1, column=0)
+        self.prev_button.grid(row=2, column=0)
         #----slider init----#
         self.slider = tk.Scale(self.frame, from_=0, to=0, orient=tk.HORIZONTAL, length=200, command=self.update_image_index)
-        self.slider.grid(row=1, column=1)
+        self.slider.grid(row=2, column=1)
         #----slider----#
         #----button----#
         self.next_button = tk.Button(self.frame, text="NEXT", width=7, command=self.show_next_image)
-        self.next_button.grid(row=1,column=2)
+        self.next_button.grid(row=2,column=2)
         #----button----#
-        self.root.resizable(True, True)
-        #------global variables-----#
+        self.root.resizable(False, False)
+        #----global variables-----#
         self.image_index = 0
         self.images = []
+        self.converted_images = []
         self.file_objects = []
         self.photo_image = None
         self.var = 0
-    def open_dicom(self):
+    def create_menu(self):
+        #----menubar----#
+        self.menubar = tk.Menu(self.root)
+        #----menu1----#
+        self.menu1 = tk.Menu(self.menubar, tearoff=0)
+        #----menu1 submenu----#
+        self.menu1_submenu = tk.Menu(self.menu1, tearoff=0)
+        self.menu1_submenu.add_command(label="File", command=self.open_dicom_file)
+        self.menu1_submenu.add_command(label="Directory", command=self.open_folder)
+        #----menu1 submenu----#
+        self.menu1.add_cascade(label="Import", menu=self.menu1_submenu)
+        self.menu1.add_separator()
+        self.menu1.add_command(label="Close", command=self.close)
+        #----menu2----#
+        self.menu2 = tk.Menu(self.menubar, tearoff=0)
+        self.menu2.add_command(label="Invert", command=self.invert)
+        #----menu3----#
+        self.menu3 = tk.Menu(self.menubar, tearoff=0)
+        self.menu3.add_command(label="Info", command=self.show_info_window)
+        #----menu4----#
+        self.menu4 = tk.Menu(self.menubar, tearoff=0)
+        self.menu4.add_command(label="Cont HU", command=self.show_control_HU_window)
+        #----menubar----#
+        self.menubar.add_cascade(label="File",menu=self.menu1)
+        self.menubar.add_cascade(label="View", menu=self.menu2)
+        self.menubar.add_cascade(label="ProcWindow", menu=self.menu4)
+        self.menubar.add_cascade(label="Help", menu=self.menu3)
+        self.root.config(menu=self.menubar)
+        #----end of menubar----#
+    def open_dicom_file(self):
+        '''
+        open a dicom file
+        '''
         file_path = filedialog.askopenfilename(title="Select DICOM file", filetypes=(("DICOM files", "*.dcm"), ("All files", "*.*")))
         if file_path:
             self.images = self.load_dicom_images(file_path)
             self.show_image()
     def open_folder(self):
+        '''
+        open dicom files from folder
+        '''
         folder_path = filedialog.askdirectory(title="Select Folder")
         if folder_path:
             self.images = self.load_dicom_folder(folder_path)
             self.show_image()    
     def load_dicom_images(self, file_path):
+        '''
+        load a dicom file
+        '''
         try:
             dicom_data = pydicom.dcmread(file_path)
             self.file_objects = [dicom_data]
@@ -75,6 +112,9 @@ class SimpleDicomViewer:
             print("Error Loading DICOM file:", e)
             return []
     def load_dicom_folder(self, folder_path):
+        '''
+
+        '''
         try:
             self.file_objects = [pydicom.dcmread(elem) for elem in glob(folder_path+"\\*.dcm")]
             self.images = [elem2.pixel_array for elem2 in self.file_objects]
@@ -104,15 +144,15 @@ class SimpleDicomViewer:
         if temp_fObj.Modality == "CT":
             temp_image = temp_image*temp_fObj.RescaleSlope+temp_fObj.RescaleIntercept
         self.photo_image = self.convert_to_image(temp_image)
-        self.canvas.config(width=self.photo_image.width(), height=self.photo_image.height())
         self.canvas.create_image(0, 0, anchor=tk.NW, image=self.photo_image)
-        self.prev_button.grid(row=1, column=0)
+        self.canvas.config(scrollregion=self.canvas.bbox("all"), confine=True)
+        self.prev_button.grid(row=2, column=0)
         # slider part
         self.slider.config(from_=0, to=len(self.images)-1)
         self.slider.set(self.image_index)
-        self.slider.grid(row=1, column=1)
+        self.slider.grid(row=2, column=1)
         # slider part
-        self.next_button.grid(row=1, column=2)
+        self.next_button.grid(row=2, column=2)
     def normalize(self, input):
         if np.max(input) != 0:
             maxValue = np.max(input)

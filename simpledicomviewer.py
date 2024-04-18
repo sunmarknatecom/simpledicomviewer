3c6c81098ee7cf16c1e7a26b88dbd5afe5b51dc8
Mark S. Hong <33437440+sunmarknatecom@users.noreply.github.com>
2024-04-18 14:06:31 +0900

    normalize function
    의료영상의 특성상 신호를 영상으로 출력한 것이기 때문에, 기본적으로 출력신호는 1채널로 구성됩니다. 각각의 픽셀의 값이 한 개의 값을 나타낸다는 뜻입니다. 앞에서 np.max()와 np.min()메서드들로 영상의 최대값과 최소값을 갖는 픽셀들의 값을 출력했었습니다. 위의 출력 화면은 사실 픽셀의 최대값은 255로 재조정되고 가장 밝은 흰색이 되면, 가장 낮은 np.min()값은 0으로 재조정되고 가장 어두운 검은색이 됩니다. 중간의 값은 0~255사이의 정수값으로 재조정되며, 각각의 픽셀 밝기로 표현됩니다. 이런 변환은 정규화(normalization)라고 말하며, 일반적인 수식은 다음과 같습니다.
    xnew = (x-xmin)/(xmax-xmin)
    위의 수식을 코드로 구현합니다. 
    의료영상의 특성상 신호를 영상으로 출력한 것이기 때문에, 기본적으로 출력신호는 1채널로 구성됩니다. 각각의 픽셀의 값이 한 개의 값을 나타낸다는 뜻입니다. 앞에서 np.max()와 np.min()메서드들로 영상의 최대값과 최소값을 갖는 픽셀들의 값을 출력했었습니다. 위의 출력 화면은 사실 픽셀의 최대값은 255로 재조정되고 가장 밝은 흰색이 되면, 가장 낮은 np.min()값은 0으로 재조정되고 가장 어두운 검은색이 됩니다. 중간의 값은 0~255사이의 정수값으로 재조정되며, 각각의 픽셀 밝기로 표현됩니다. 이런 변환은 정규화(normalization)라고 말하며, 일반적인 수식은 다음과 같습니다.

--- simpledicomviewer.py
+++ simpledicomviewer.py
@@ -7,10 +7,15 @@ file_path = "path"
 try:
     file_object = pydicom.dcmread(file_path)
     image_array = file_object.pixel_array
-    if len(np.shape(temp_images)) !=3:
+    if len(np.shape(image_array)) !=3:
         temp_array = np.array([image_array])
     else:
         temp_array = image_array
 except Exception as e:
     print("Error loading DICOM file: ", e)
     temp_array = []
+
+def normalize(input_frame):
+    max_value = np.max(input_frame)
+    min_value = np.min(input_frame)
+    return np.array(((input_frame-min_value)/(max_value-min_value))*255, dtype=np.uint8)

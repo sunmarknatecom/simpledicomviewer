3bf37ea1b425a6c8566017715665e1e19ee2f922
Mark S. Hong <33437440+sunmarknatecom@users.noreply.github.com>
2024-04-18 13:47:17 +0900

    backbone code
    그럼 지금까지 작성한 코드들로 뼈대가 되는 코드를 작성하도록 하겠습니다. 짧은 코드이기는 하지만, 많은 것들을 이해해야 이 코드를 실행할 수 있습니다. 사실 DICOM파일은 한 개 이상의 영상들을 담고 있을 수 있습니다. CT와 같은 단층촬영 영상들은 대부분 하나의 DICOM파일에 한 개의 영상을 넣기도 하지만, 여러 장의 전체 영상을 한 개의 파일에 넣을 수도 있습니다. 따라서 DICOM 파일이 한 개의 영상을 가지고 있는지, 아니면 여러 개의 영상을 가지고 있는지를 확인하는 과정이 필요합니다.

--- simpledicomviewer.py
+++ simpledicomviewer.py
@@ -1,45 +1,11 @@
-import pydicom, os
+import pydicom
 import numpy as np
-from pydicom.data import get_testdata_file
 from PIL import Image
 
-filename_mr = get_testdata_file("MR_small.dcm")
-filename_ct = get_testdata_file("CT_small.dcm")
+file_path = "path"
 
-print(filename_mr)
-print(filename_ct)
+file_object = pydicom.dcmread(file_path)
+image_array = file_object.pixel_array
 
-# 예제파일들의 경로
-print(os.path.dirname(filename_mr))
-
-# 예제파일들을 확인
-print(os.listdir(os.path.dirname(filename_mr)))
-
-mr_object = pydicom.dcmread(filename_mr)
-ct_object = pydicom.dcmread(filename_ct)
-
-mr_image = mr_object.pixel_array
-ct_image = ct_object.pixel_array
-
-print(type(mr_image))
-print(type(ct_image))
-
-print(np.shape(mr_image))
-#(65,64)
-print(np.shape(ct_image))
-#(128,128)
-
-print("MR_max_value", np.max(mr_image), ", ", "MR_min_value", np.min(mr_image))
-print("CT_max_value", np.max(ct_image), ", ", "CT_min_value", np.min(ct_image))
-
-mr_PIL_image = Image.fromarray(mr_image)
-mr_PIL_image.show()
-
-ct_PIL_image = Image.fromarray(ct_image)
-ct_PIL_image.show()
-
-plt.imshow(mr_object, cmap='gray')
-plt.show()    # 그림3 출력 - 출력창의 오른쪽 위 X탭 클릭 후 창을 닫음
-
-plt.imshow(ct_object, cmap='gray')
-plt.show()    # 그림4 출력 - 출력창의 오른쪽 위 X탭 클릭 후 창을 닫음
+show_image = Image.fromarray(image_array)
+show_image.show()
